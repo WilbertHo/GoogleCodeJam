b8aecc1a06dbfb3c667ebbffe832b0ef5524ba59
Wilbert Ho <wilbert.ho@gmail.com>
2013-10-20 21:51:05 +1300

    Working Python version of Alien Numbers.

--- PracticeContests/PracticeProblems/AlienNumbers/py/translator.py
+++ PracticeContests/PracticeProblems/AlienNumbers/py/translator.py
@@ -1,18 +1,90 @@
 #!/usr/bin/python
+import argparse
 import sys
 
-class Translator(object):
-    def __init__(self, languages=None):
-        languages = languages or []
 
-        # First line is number of test cases
-        numCases = languages.pop(0)
+class Translator(object):
+    """ Args:
+            languages: string containing source number, source language
+                       and target language
+    """
+    def __init__(self, languages=None):
+        self.languages = languages or []
+
+        # split the input into source number, source and target language
+        self.sourceNum, self.sourceLang, self.targetLang = self.languages.split()
+        self.sourceRadix = len(self.sourceLang)
+        self.targetRadix = len(self.targetLang)
+
+    def __str__(self):
+        print "{src} {srcL} {tgtL}".format(src=self.sourceNum,
+                                           srcL=self.sourceLang,
+                                           tgtL=self.targetLang)
+
+    def __repr__(self):
+        print "{cls}({params})".format(cls=self.__class__.__name__,
+                                       params=self.languages)
+
+    def sourceValue(self):
+        """ Get the value of source number """
+        sourceNum = self.sourceNum
+        sourceLang = self.sourceLang
+        sourceRadix = self.sourceRadix
+
+        # translate source number into an actual value
+        num = 0
+        # reverse so we can easily multiply easily
+        for mult, digit in enumerate(sourceNum[::-1]):
+            # Ex: 19 (0123456789) is 9 * 10^0 + 1 * 10^1
+            num += sourceLang.index(digit) * (sourceRadix ** mult)
+
+        # translate to the target language
+
+        return num
 
     def translate(self):
-        pass
+        """ Translate the source number into the target language """
+        sourceValue = self.sourceValue()
+        targetLang = self.targetLang
+        radix = self.targetRadix
+        targetNum = []
+
+        # Divide by the target radix and keep the remainder
+        num = sourceValue
+
+        while(num > 0):
+            # Get the remainder of dividing number by radix
+            # Ex: 9 in oF8 (radix: 3) should be Foo
+            # 9 % 3 == 0, 9/3 == 3 -> 3 % 3 == 0, 3/3 == 1 -> 1 % 3 == 1
+            targetNum.append(targetLang[num % radix])
+
+            # Move to the next digit
+            num = num / radix
+
+        # Since we constructed the target number starting from the smallest
+        # digit, reverse before returning
+        return ''.join(targetNum[::-1])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-file', dest='file', help='Input file')
+    pargs = parser.parse_args()
+
+    if not pargs.file:
+        print 'Specify an input file'
+        sys.exit()
+
+    with open(pargs.file, 'r') as f:
+        languages = [line.rstrip() for line in f.readlines()]
+
+    # First line is number of test cases
+    numCases = languages.pop(0)
+
+    for caseNum, language in enumerate(languages, 1):
+        print "Case #{caseNum}: {lang}".format(
+            caseNum=caseNum,
+            lang=Translator(language).translate())
 
 if __name__ == '__main__':
-    f = open(sys.argv[1], 'r')
-    languages = f.readlines()
-    f.close()
-    translator = Translator(languages)
+    main()
